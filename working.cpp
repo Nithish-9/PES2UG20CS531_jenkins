cba0c531ce789acde44de89d827e7c17495f1f1f
Nithish-9 <113118468+Nithish-9@users.noreply.github.com>
2023-02-17 19:53:24 +0530

    Update and rename Jenkinsfile.cpp to working.cpp

---
+++ working.cpp
@@ -0,0 +1,11 @@
+//File: working.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  int a = 5;
+  int b = 9;
+  cout <<"Multiply two numbers : "<<a*b<<"\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
